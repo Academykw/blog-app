901e8f15bb1530a38f32b5e74966ac58bad4921e
Aliu usman <cadetkw40@gmail.com>
2024-08-14 14:43:33 +0100

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:class1/core/Secret/app_secret.dart';
 import 'package:class1/core/theme/theme.dart';
+import 'package:class1/features/auth/data/auth_remote_data_source.dart';
+import 'package:class1/features/auth/data/auth_repository_impl.dart';
+import 'package:class1/features/auth/domain/user_sign_up.dart';
+import 'package:class1/features/auth/presentation/auth_bloc.dart';
 import 'package:class1/features/auth/presentation/pages/login_page.dart';
 import 'package:class1/features/auth/presentation/pages/signup_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
@@ -10,7 +15,16 @@ void main() async {
    final supaBase = await Supabase.initialize(
       url: AppSecret().superBaseUrl,
       anonKey: AppSecret().superBaseApiKey);
-  runApp(const MyApp());
+
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider(create: (_) =>  AuthBloc(userSignUp: UserSignUp(AuthRepositoryImpl
+        (AuthRemoteDataSourceImpl(
+          supaBase.client)
+        ,),),),)
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
